c485654574201026ee1a5c8f700d1257019c5a87
Dmitry Dudarev <dr.nt2017@yandex.ru>
2022-12-10 20:51:22 +0300

    сделано автозаполнение

--- script/index.js
+++ script/index.js
@@ -1,8 +1,21 @@
-const  profileEditButton = document.querySelector('.profile__editbutton');
+const profileEditButton = document.querySelector('.profile__editbutton');
 console.log (typeof profileEditButton);
 
-const  editPopup = document.querySelector('.edit_popup');
+const editPopup = document.querySelector('.edit_popup');
 console.log (typeof editPopup);
+
+let userName = document.querySelector('.profile__username');
+console.log (typeof userName);
+
+let aboutUser = document.querySelector('.profile__aboutuser');
+console.log (typeof aboutUser);
+
+let editUserName = document.querySelector('.popup__input-name');
+editUserName.placeholder = userName.textContent
+let editUserJob = document.querySelector('.popup__input-job');
+editUserJob.placeholder = aboutUser.textContent
+
+
 
 // function openPopup () {
 //   editPopup.classList.add('popup_opened');
