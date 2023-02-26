bc647f3c110b7fbde03ece9a4c5d63a9aefdfe6b
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-02-26 12:58:07 +0300

    refact: переименованы переменные, фрагментированы объемные функции

--- script/index.js
+++ script/index.js
@@ -1,81 +1,92 @@
 const profileEditButton = document.querySelector('.profile__editbutton');
 let userName = document.querySelector('.profile__username');
 let aboutUser = document.querySelector('.profile__aboutuser');
-const editPopup = document.querySelector('.popup');
+const popup = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__close-button');
 const savePopupButton = document.querySelector('.popup__save-button');
 let formElement = document.querySelector('.popup__input-userdata');
-let editUserName = document.querySelector('.popup__input_type_name');
-// editUserName.placeholder = userName.textContent;
-let editUserJob = document.querySelector('.popup__input_type_job');
-// editUserJob.placeholder = aboutUser.textContent;
+const popupTitle = document.querySelector('.popup__title');
+const popupDesignation = document.querySelector('.popup__input_designation');
+const popupProperty = document.querySelector('.popup__input_property');
+const addButton = document.querySelector('.profile__addbutton');
 
-profileEditButton.addEventListener('click', () => {
-  editUserName.placeholder = userName.textContent;
-  editUserJob.placeholder = aboutUser.textContent;
-  editPopup.setAttribute('profile','');
-  document.querySelector('.popup__title').textContent = 'Редактировать профиль';
-  editUserName.value = userName.textContent;
-  editUserJob.value = aboutUser.textContent;
-  editPopup.classList.add('popup_opened');
-  savePopupButton.textContent = 'Сохранить';
-});
+
+const profile = {
+    title: 'Редактировать профиль',
+    button: 'Сохранить',
+};
+
+const card = {
+    title: 'Новое место',
+    designation: 'Название',
+    property: 'Ссылка на картинку',
+    button: 'Сохранить',
+};
+
+// profileEditButton.addEventListener('click', () => {
+//   popup.setAttribute('data-profile',''); - 
+//   popup.classList.add('popup_opened'); - 
+//   popupDesignation.placeholder = userName.textContent;
+//   popupProperty.placeholder = aboutUser.textContent;
+//   document.querySelector('.popup__title').textContent = 'Редактировать профиль';
+//   popupDesignation.value = userName.textContent;
+//   editUserJob.value = aboutUser.textContent;
+//   savePopupButton.textContent = 'Сохранить';
+// });
+
+
+profileEditButton.addEventListener('click', editProfile);
+
+function editProfile () {
+  popup.setAttribute('data-profile','');
+  togglePopup();
+  createProfileEditor();
+}
+
+function createProfileEditor () {
+  popupDesignation.placeholder = userName.textContent;
+  popupProperty.placeholder = aboutUser.textContent;
+  popupTitle.textContent = profile.title;
+  popupDesignation.value = userName.textContent;
+  popupProperty.value = aboutUser.textContent;
+  savePopupButton.textContent = profile.button;
+}
+
+function togglePopup () {
+  popup.classList.toggle('popup_opened');
+}
 
 closePopupButton.addEventListener('click', () => {
-  editPopup.removeAttribute('profile');
-  editPopup.classList.remove('popup_opened');
+  popup.removeAttribute('data-profile');
+  togglePopup();
 });
 
 formElement.addEventListener('submit', handleFormSubmit);
 function handleFormSubmit (evt) {
   evt.preventDefault();
-  if (editPopup.hasAttribute('profile')) {
-    userName.textContent = editUserName.value;
-    aboutUser.textContent = editUserJob.value;
-    editPopup.classList.remove('popup_opened');
-    editPopup.removeAttribute('profile')
-    editUserName.textContent = '';
-    editUserJob.textContent = '';
+  if (popup.hasAttribute('data-profile')) {
+    userName.textContent = popupDesignation.value;
+    aboutUser.textContent = popupProperty.value;
+    popup.removeAttribute('data-profile');
+    togglePopup();
+
   } else {
-    editPopup.classList.remove('popup_opened');
-    alert(`название карточки: ${editUserName.value};
-           Ссылка на картинку: "${editUserJob.value}"`);
+    popup.classList.remove('popup_opened');
+    alert(`название карточки: ${popupDesignation.value};
+           Ссылка на картинку: "${popupProperty.value}"`);
   }
 }
 
 
-
-
-
-
-
-
-
-const addButton = document.querySelector('.profile__addbutton');
-
 addButton.addEventListener ('click', openAddCardForm);
 function openAddCardForm () {
-  editUserName.placeholder = 'Название';
-  editUserJob.placeholder = 'Ссылка на картинку';
-  editPopup.classList.add('popup_opened');
+  popupDesignation.placeholder = 'Название';
+  popupProperty.placeholder = 'Ссылка на картинку';
+  popup.classList.add('popup_opened');
   document.querySelector('.popup__title').textContent = 'Новое место';
-  editUserName.value = '';
-  editUserJob.value = '';
+  popupDesignation.value = '';
+  popupProperty.value = '';
   savePopupButton.textContent = 'Создать';
 
 }
 
-// const popupModels = [
-//   {
-//     title: 'Редактировать профиль',
-//     designation,
-//     property,
-//     button: 'Сохранить',
-//   },
-//   {
-//     title: 'Новое место',
-//     designation: 'Название',
-//     property: 'Ссылка на картинку',
-//     button: 'Сохранить',
-//   }
-// ]
