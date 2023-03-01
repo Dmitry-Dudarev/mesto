538dc9053178ee5c52a51461c9f76cf2664f5bf1
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-03-01 20:17:16 +0300

    refact: переделана функция открытия попапа с кртинкой

--- script/index.js
+++ script/index.js
@@ -18,6 +18,8 @@ const cardLink = document.querySelector('.card-creator__link');
 const addButton = document.querySelector('.profile__addbutton');
 const elements = document.querySelector('.elements');
 const picture = document.querySelector('.picture');
+const pictureImage = picture.querySelector('.picture__image');
+const pictureFigcaption = picture.querySelector('.picture__figcaption');
 const card = {};
 let newCard;
 
@@ -75,54 +77,25 @@ function createCard(elem) {
   const preCard = cardTemplate.cloneNode(true);
   preCard.querySelector('.element__text').textContent = elem.name;
   preCard.querySelector('.element__image').src = elem.link;
-  preCard.querySelector('.element__image').alt = `На фото: ${elem.name}`;
-  preCard.querySelector('.element__image').addEventListener('click', openPicture);
+  preCard.querySelector('.element__image').alt = elem.name;
+  preCard.querySelector('.element__image').addEventListener('click', (evt) => openPicture(evt.target));
   preCard.querySelector('.element__trash').addEventListener('click', deleteCard);
   preCard.querySelector('.element__reaction').addEventListener('click', reactToACard);
   newCard = preCard;
-}
-
-
-// тестируем функцию создания карточки!
-
-// universalAddCard(cards[0]);
-// console.log(redyCard);
-// console.log(typeof(redyCard));
-
-// добавление карточки в разметку
+};
 
 function addCard () {
   elements.prepend(newCard);
 };
 
-
-
-
-
-// старый код переработан
-// function addNewCard() {
-//   const cardTemplate = document.querySelector('.cardTemplate').content.cloneNode(true);
-//   cardTemplate.querySelector('.element__text').textContent = cards[0].name;
-//   cardTemplate.querySelector('.element__image').src = cards[0].link;
-//   cardTemplate.querySelector('.element__image').addEventListener('click', openPicture);
-//   cardTemplate.querySelector('.element__trash').addEventListener('click', deleteCard);
-//   cardTemplate.querySelector('.element__reaction').addEventListener('click', reactToACard);
-//   elements.prepend(cardTemplate);
-// };
-
 function deleteCard(evt) {
   evt.target.closest('.element').remove();
 };
 
-function openPicture(evt) {
-  const elem = evt.target.closest('.element')
+function openPicture(data) {
   picture.classList.add('picture_opened');
-  // добавляет адрес ссылки на изображение
-  // в форму для вывода полноразмерного изображения (по клику на его карточку)
-  document.querySelector('.picture__image').src = evt.target.src;
-  // добавляет подпись к изображению
-  const pictureText = elem.querySelector('.element__text').textContent;
-  document.querySelector('.picture__figcaption').textContent = pictureText;
+  pictureImage.src = data.src;
+  pictureFigcaption.textContent = data.alt;
 };
 
 document.querySelector('.picture__close-button').addEventListener('click', () => {
