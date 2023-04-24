53678ee23b8a90ef85bdf8d7e8aea880bdd66627
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-04-24 23:10:23 +0300

    refact: удален старый код из PopupWithImage

--- script/PopupWithImage.js
+++ script/PopupWithImage.js
@@ -1,25 +1,15 @@
 import { Popup } from "./Popup.js";
 
 export class PopupWithImage extends Popup {
-  // в конструктор надо передать элемент изображения 
-  //данные которого будут в методе open заполняться в 
-  //формы попапа
   constructor(selector) {
     super(selector);
-    
   }
 
-  open(data){
+  open(data) {
     super.open();
-    // теперь нужны селекторы полей попапа с картинкой
-    //чтобы по ним найти поля, а в эти поля вставить данные 
-    //переданной картинки
-
-    //вместо дата нужно имя переменной с фоткой
     const popupImage = this._popup.querySelector('.popup__image');
     popupImage.src = data.src;
     popupImage.alt = `На фото: ${data.alt}`;
-    this._popup.querySelector('.popup__figcaption').textContent = data.alt; 
+    this._popup.querySelector('.popup__figcaption').textContent = data.alt;
   }
-  
-}
+};
