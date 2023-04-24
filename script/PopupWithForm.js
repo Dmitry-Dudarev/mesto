2fc372c598c099084c4dad69e0b2f69293baadac
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-04-24 23:12:14 +0300

    refact: удален старый код из PopupWithForm

--- script/PopupWithForm.js
+++ script/PopupWithForm.js
@@ -2,20 +2,16 @@ import { Popup } from "./Popup.js";
 
 export class PopupWithForm extends Popup {
   constructor(selector, handleSubmitForm) {
-    // handleSubmitForm это должен быть колбэк сабмита формы
     super(selector);
     this._handleSubmitForm = handleSubmitForm;
     this._popupForm = this._popup.querySelector('.popup__form');
   }
 
-  
   close() {
     super.close();
     this._popupForm.reset();
   }
 
-
-  // собираем данные всех полей формы
   _getInputValues() {
     this._inputList = this._popup.querySelectorAll('.popup__input');
     this._formValues = {};
@@ -26,13 +22,10 @@ export class PopupWithForm extends Popup {
 
   setEventListeners() {
     super.setEventListeners();
-    //добавить обработчик сабмита формы
-
     this._popupForm.addEventListener('submit', (evt) => {
       evt.preventDefault();
       this._data = this._getInputValues();
       this._handleSubmitForm(this._data);
-
     })
   }
 }
