fdec28ed9feefcf5b22fcc1ae5d41f5d5ff36275
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-04-26 21:10:50 +0300

    refact:  добавлена папка src

---
+++ src/components/cards.js
@@ -0,0 +1,26 @@
+export const cards = [
+  {
+    name: 'Волга',
+    link: 'src/images/element__volga_fedor-shlyapnikov.jpg',
+  },
+  {
+    name: 'Карелия',
+    link: 'src/images/element__karelia_egor-myznik.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'src/images/element__baykal_kir-simakov.jpg',
+  },
+  {
+    name: 'Домбай',
+    link: 'src/images/element__dombay_kirill-pershin.png',
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: 'src/images/element__elbrus_kirill-pershin.png',
+  },
+  {
+    name: 'Карачаево-Черкессия',
+    link: 'src/images/element__karachaevsk_kirill-pershin.jpg',
+  },
+];
